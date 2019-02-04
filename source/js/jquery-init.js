0ecd7a1d425bf316fb1df4b22b0feaa90faf4250
Ivan <proekt.energetika@gmail.com>
2019-02-04 22:02:11 +0300

    Разметил, стилизовал и адаптировал блок promo

---
+++ source/js/jquery-init.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  $(document).ready(function(){
+  $('.promo__list').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
+})();
