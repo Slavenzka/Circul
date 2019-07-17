dccb5040848d2a173a8a68abca7defaae9073059
Ivan <i.romanov@protonmail.com>
2019-07-17 20:00:28 +0300

    Внес правки в слайдер на главной

--- source/js/jquery-init.js
+++ source/js/jquery-init.js
@@ -2,12 +2,27 @@
   'use strict';
 
   $(document).ready(function(){
-  $('.promo__list').slick({
-    arrows: false,
-    dots: true,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 2000
-  });
+    $('.promo__list').slick({
+      arrows: false,
+      dots: true,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 300
+    });
+    $('.promo__list').on('wheel', (function(e) {
+      e.preventDefault();
+      let scrollCount = 0;
+
+      scroll = setTimeout(function () { scrollCount = 0; }, 200);
+      if (scrollCount) return 0;
+      scrollCount = 1;
+
+      if (e.originalEvent.deltaY < 0) {
+        $(this).slick('slickPrev');
+      } else {
+        $(this).slick('slickNext');
+      }
+    }));
 });
 })();
